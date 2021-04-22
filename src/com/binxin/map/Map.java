ba1561cae46d9d19da02cdfcab0c40efca25fd94
chenb <444921684@qq.com>
2021-04-22 16:45:12 +0800

    Map

---
+++ src/com/binxin/map/Map.java
@@ -0,0 +1,22 @@
+package com.binxin.map;
+
+/**
+ * @author chen bin
+ * @date 2021/4/22
+ */
+public interface Map<K, V> {
+
+    void add(K key, V value);
+
+    V remove(K key);
+
+    boolean contains(K key);
+
+    V get(K key);
+
+    void set(K key, V value);
+
+    int getSize();
+
+    boolean isEmpty();
+}
