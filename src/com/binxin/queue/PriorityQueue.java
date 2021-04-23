d9cdad63abd21916c327cd800f90481819a8e31c
chenb <444921684@qq.com>
2021-04-23 10:21:04 +0800

    PriorityQueue

---
+++ src/com/binxin/queue/PriorityQueue.java
@@ -0,0 +1,41 @@
+package com.binxin.queue;
+
+import com.binxin.maxheap.MaxHeap;
+
+/**
+ * @author chen bin
+ * @date 2021/4/23
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+}
